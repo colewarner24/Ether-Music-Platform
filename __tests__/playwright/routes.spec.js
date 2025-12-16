a974e164382c0680d1b0173fc2f07db5f4d1b34e
colewarner24 <74276573+colewarner24@users.noreply.github.com>
2025-12-16 14:38:42 -0800

    playwright init

---
+++ __tests__/playwright/routes.spec.js
@@ -0,0 +1,7 @@
+import{ test, expect } from "@playwright/test";
+
+test("Home page render correctly", async({ page })=>{
+    await page.goto("http://localhost:3000/tracks");
+    await expect(page.getByRole('link', { name: 'the ether' })).toHaveText(/the ether/);
+    await expect(page.getByRole('heading', { name: 'Tracks' })).toBeVisible();
+});
